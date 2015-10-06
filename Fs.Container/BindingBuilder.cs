e10dce5ed57a1466a4c3f245237e8f0e6157b192
FSou1 <my.fsou1@gmail.com>
2015-10-06 23:08:59 +0300

    Base version init

---
+++ Fs.Container/BindingBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fs.Container {
+    public class BindingBuilder {
+        internal Type _service;
+        internal Type _concrete;
+        internal Dictionary<string, object> _arguments;
+
+        public BindingBuilder(Type service) {
+            _service = service;
+        }
+
+        public BindingBuilder Use<T>() {
+            _concrete = typeof(T);
+            return this;
+        }
+
+        public BindingBuilder WithConstructorArgument(string argumentName, object argumentValue) {
+            if(_arguments == null)
+                _arguments = new Dictionary<string, object>();
+
+            _arguments[argumentName] = argumentValue;
+            return this;
+        }
+    }
+}
---
+++ Fs.Container/FsContainer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fs.Container {
+    public class FsContainer {
+        internal List<BindingBuilder> _bindingBuilders = new List<BindingBuilder>();
+
+        public T Resolve<T>() {
+            return (T)Resolve(typeof (T));
+        }
+
+        public object Resolve(Type type) {
+            var builder = this._bindingBuilders.FirstOrDefault(t => t._service == type);
+            if (builder != null) {
+                return CreateInstance(builder);
+            }
+
+            return CreateInstance(type);
+        }
+
+        public object CreateInstance(BindingBuilder builder) {
+            var concrete = builder._concrete;
+            var arguments = builder._arguments ?? new Dictionary<string, object>();
+
+            return CreateInstance(concrete, arguments);
+        }
+
+        public object CreateInstance(Type concrete) {
+            if (concrete.GetConstructor(Type.EmptyTypes) != null) {
+                return Activator.CreateInstance(concrete);
+            }
+
+            return CreateInstance(concrete, new Dictionary<string, object>());
+        }
+
+        public object CreateInstance(Type concrete, IDictionary<string, object> constructorArguments) {
+            var ctor = new ConstructorScorer(concrete, constructorArguments).GetConstructor();
+
+            var parameters = ctor.GetParameters();
+            var arguments = new object[parameters.Length];
+            for (var i = 0; i < parameters.Length; i++) {
+                var parameter = parameters[i];
+                var argument = constructorArguments.ContainsKey(parameter.Name)
+                    ? constructorArguments[parameter.Name]
+                    : this.Resolve(parameter.ParameterType);
+
+                arguments[i] = argument;
+            }
+
+            return ctor.Invoke(arguments);
+        }
+
+        public BindingBuilder For<T>() {
+            var type = typeof(T);
+            
+            var bindingBuilder = new BindingBuilder(type);
+            _bindingBuilders.Add(bindingBuilder);
+
+            return bindingBuilder;
+        }
+    }
+}
