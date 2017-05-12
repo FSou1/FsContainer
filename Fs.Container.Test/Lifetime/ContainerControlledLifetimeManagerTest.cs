7a014680ab153d1b8b7fbde462561278233cc2bb
FSou1 <my.fsou1@gmail.com>
2017-05-12 18:42:21 +0300

    Remove constructor initialization from container controlled test (small useless cleanup)

--- Fs.Container.Test/Lifetime/ContainerControlledLifetimeManagerTest.cs
+++ Fs.Container.Test/Lifetime/ContainerControlledLifetimeManagerTest.cs
@@ -6,37 +6,42 @@ using System.Threading.Tasks;
 namespace Fs.Container.Test.Lifetime {
     [TestClass]
     public class ContainerControlledLifetimeManagerTest {
-        private IFsContainer child1;
-        private IFsContainer child2;
-        private IFsContainer parent;
-
-        public ContainerControlledLifetimeManagerTest()
-        {
-            parent = new FsContainer();
-            parent.For<ILogger>()
-                .Use<Logger>(new ContainerControlledLifetimeManager());
-            parent.For<DisposableObject>()
-                .Use<DisposableObject>(new ContainerControlledLifetimeManager());
-            child1 = parent.CreateChildContainer();
-            child2 = parent.CreateChildContainer();
-        }
-
         [TestMethod]
         public async Task TestContainerControlledLifetimeInstanceAlwaysSame() {
             // Arrange
+            var parent = new FsContainer();
+
+            parent
+                .For<ILogger>()
+                .Use<Logger>(new ContainerControlledLifetimeManager());
+
+            // Act
             var logger = await parent.ResolveAsync<ILogger>();
             var logger1 = await parent.ResolveAsync<ILogger>();
+            var logger2 = await parent.ResolveAsync<ILogger>();
 
             // Assert
             Assert.IsNotNull(logger);
             Assert.IsNotNull(logger1);
+            Assert.IsNotNull(logger2);
             Assert.AreSame(logger, logger1);
-        }
+            Assert.AreSame(logger, logger2);
+        }               
 
         [TestMethod]
         public async Task ParentAndChildResolvesSameContainerControlledInstances()
         {
             // Arrange
+            var parent = new FsContainer();
+
+            parent
+                .For<ILogger>()
+                .Use<Logger>(new ContainerControlledLifetimeManager());
+
+            var child1 = parent.CreateChildContainer();
+            var child2 = parent.CreateChildContainer();
+
+            // Act
             var logger = await parent.ResolveAsync<ILogger>();
             var logger1 = await child1.ResolveAsync<ILogger>();
             var logger2 = await child2.ResolveAsync<ILogger>();
@@ -53,14 +58,26 @@ namespace Fs.Container.Test.Lifetime {
         [TestMethod]
         public async Task MultipleResolvedInstanceDisposeOnlyOnce()
         {
+            // Arrange
+            var parent = new FsContainer();
+
+            parent
+                .For<DisposableObject>()
+                .Use<DisposableObject>(new ContainerControlledLifetimeManager());
+
+            // Act
             var o1 = await parent.ResolveAsync<DisposableObject>();
             var o2 = await parent.ResolveAsync<DisposableObject>();
+            var o3 = await parent.ResolveAsync<DisposableObject>();
 
+            // Assert
             parent.Dispose();
             Assert.IsTrue(o1.WasDisposed);
             Assert.IsTrue(o2.WasDisposed);
+            Assert.IsTrue(o3.WasDisposed);
             Assert.AreEqual(o1.DisposeCount, 1);
             Assert.AreEqual(o2.DisposeCount, 1);
+            Assert.AreEqual(o3.DisposeCount, 1);
         }
     }
 }
